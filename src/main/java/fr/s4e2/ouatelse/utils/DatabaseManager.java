408f57d62898611c5e926b94680b27313e360c59
sitalbi <simon0111@live.fr>
2021-02-10 15:01:20 +0100

    added Products, Providers and Stores

--- src/main/java/fr/s4e2/ouatelse/utils/DatabaseManager.java
+++ src/main/java/fr/s4e2/ouatelse/utils/DatabaseManager.java
@@ -5,6 +5,9 @@ import com.j256.ormlite.dao.GenericRawResults;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import fr.s4e2.ouatelse.objects.Products;
+import fr.s4e2.ouatelse.objects.Providers;
+import fr.s4e2.ouatelse.objects.Stores;
 import fr.s4e2.ouatelse.objects.User;
 import lombok.Getter;
 
@@ -32,6 +35,9 @@ public class DatabaseManager {
 
     public void setupTables() throws SQLException {
         TableUtils.createTableIfNotExists(connectionSource, User.class);
+        TableUtils.createTableIfNotExists(connectionSource, Providers.class);
+        TableUtils.createTableIfNotExists(connectionSource, Products.class);
+        TableUtils.createTableIfNotExists(connectionSource, Stores.class);
     }
 
     public void displayTables() throws SQLException {
