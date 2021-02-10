e7260f9b8d28d2cb805df48ff9c3723f1b5a20cf
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-02-10 15:31:04 +0100

    Fixed up setupTables.

--- src/main/java/fr/s4e2/ouatelse/utils/DatabaseManager.java
+++ src/main/java/fr/s4e2/ouatelse/utils/DatabaseManager.java
@@ -5,10 +5,7 @@ import com.j256.ormlite.dao.GenericRawResults;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-import fr.s4e2.ouatelse.objects.Products;
-import fr.s4e2.ouatelse.objects.Providers;
-import fr.s4e2.ouatelse.objects.Stores;
-import fr.s4e2.ouatelse.objects.User;
+import fr.s4e2.ouatelse.objects.*;
 import lombok.Getter;
 
 import java.io.IOException;
@@ -34,10 +31,14 @@ public class DatabaseManager {
     }
 
     public void setupTables() throws SQLException {
-        TableUtils.createTableIfNotExists(connectionSource, User.class);
-        TableUtils.createTableIfNotExists(connectionSource, Providers.class);
+        TableUtils.createTableIfNotExists(connectionSource, Address.class);
+        TableUtils.createTableIfNotExists(connectionSource, Availability.class);
+        TableUtils.createTableIfNotExists(connectionSource, Client.class);
         TableUtils.createTableIfNotExists(connectionSource, Products.class);
+        TableUtils.createTableIfNotExists(connectionSource, Providers.class);
+        TableUtils.createTableIfNotExists(connectionSource, Salary.class);
         TableUtils.createTableIfNotExists(connectionSource, Stores.class);
+        TableUtils.createTableIfNotExists(connectionSource, User.class);
     }
 
     public void displayTables() throws SQLException {
