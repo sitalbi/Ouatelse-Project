09821d12e7a386b0baeb9a1a43fdaf5b4998073f
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-02-13 16:09:32 +0100

    (fix) Resources folder

--- src/main/java/fr/s4e2/ouatelse/Main.java
+++ src/main/java/fr/s4e2/ouatelse/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -15,6 +17,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("mainWindow.fxml")));
+
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
         DatabaseManager databaseManager = new DatabaseManager();
         databaseManager.setupTables();
         databaseManager.close();
