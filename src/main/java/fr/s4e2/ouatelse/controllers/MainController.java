f4e54498bad4e6844e85919b0ac31b51f66ae12b
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-02-14 16:33:40 +0100

    (feat) Basic Store Management layout

--- src/main/java/fr/s4e2/ouatelse/controllers/MainController.java
+++ src/main/java/fr/s4e2/ouatelse/controllers/MainController.java
@@ -2,6 +2,7 @@ package fr.s4e2.ouatelse.controllers;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.util.Objects;
 public class MainController {
 
     private static final String WINDOW_TITLE_ROLES = "Ouatelse - Menu Gestion des Rôles";
+    private static final String WINDOW_TITLE_STORES = "Ouatelse - Menu Gestion des Magasins";
 
     public void onRolesMenuClick() throws IOException {
         FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getClassLoader().getResource("rolesMenuWindow.fxml")));
@@ -17,6 +19,15 @@ public class MainController {
         Stage stage = new Stage();
         stage.setScene(new Scene(loader.load()));
         stage.setTitle(WINDOW_TITLE_ROLES);
+        stage.show();
+    }
+
+    public void onStoresMenuClick(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("storesMenuWindow.fxml"));
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle(WINDOW_TITLE_STORES);
         stage.show();
     }
 }
