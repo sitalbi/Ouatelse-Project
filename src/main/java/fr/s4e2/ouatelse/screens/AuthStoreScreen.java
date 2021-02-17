d006fc834ec5d13d5171e686cccd8394c4bda469
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-02-17 19:32:45 +0100

    (feat) Finalized both authentication screens.

---
+++ src/main/java/fr/s4e2/ouatelse/screens/AuthStoreScreen.java
@@ -0,0 +1,8 @@
+package fr.s4e2.ouatelse.screens;
+
+public class AuthStoreScreen extends BaseScreen {
+
+    public AuthStoreScreen() {
+        super("auth_store.fxml", "Outaelse - Authentification Magasin");
+    }
+}
