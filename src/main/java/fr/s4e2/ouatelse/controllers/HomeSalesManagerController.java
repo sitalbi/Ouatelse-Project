92f250d684cc06ef2f0bcf98f5eda1d3e3f91781
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-02-17 18:44:36 +0100

    (feat) Finalized all home screens & controllers.

---
+++ src/main/java/fr/s4e2/ouatelse/controllers/HomeSalesManagerController.java
@@ -0,0 +1,27 @@
+package fr.s4e2.ouatelse.controllers;
+
+public class HomeSalesManagerController extends HomeController {
+    public void onProductsButtonClick() {
+        //todo : open products screen
+
+        System.out.println("Open products screen");
+    }
+
+    public void onClientsButtonClick() {
+        //todo : open clients screen
+
+        System.out.println("Open clients screen");
+    }
+
+    public void onSalesStatisticsButtonClick() {
+        //todo : open sales statistics screen
+
+        System.out.println("open salaray management screen");
+    }
+
+    public void onParametersButtonClick() {
+        //todo : open parameters screen
+
+        System.out.println("Open parameters screen");
+    }
+}
---
+++ src/main/java/fr/s4e2/ouatelse/screens/HomeSalesManagerScreen.java
@@ -0,0 +1,9 @@
+package fr.s4e2.ouatelse.screens;
+
+public class HomeSalesManagerScreen extends BaseScreen {
+
+    public HomeSalesManagerScreen() {
+        super("home_sales_manager.fxml", "Ouatelse - Responsable des ventes");
+        this.addStyleSheet("css/home.css");
+    }
+}
