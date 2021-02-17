b23006ef33382077afba6ef877b9642a793d618b
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-02-17 19:32:45 +0100

    (feat) Finalized both authentication screens.

---
+++ src/main/java/fr/s4e2/ouatelse/controllers/AuthUserController.java
@@ -0,0 +1,9 @@
+package fr.s4e2.ouatelse.controllers;
+
+public class AuthUserController extends BaseController {
+
+    public void onConnectionButtonClick() {
+        // todo : connection user
+        System.out.println("Connection for user");
+    }
+}
