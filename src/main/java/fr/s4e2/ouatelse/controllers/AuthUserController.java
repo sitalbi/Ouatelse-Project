15f80e7964f5c8abb21edf72a180ce603a261762
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-02-19 18:19:42 +0100

    (feat) User authentication + Store authentication (Simon)

--- src/main/java/fr/s4e2/ouatelse/controllers/AuthUserController.java
+++ src/main/java/fr/s4e2/ouatelse/controllers/AuthUserController.java
@@ -3,7 +3,28 @@ package fr.s4e2.ouatelse.controllers;
 public class AuthUserController extends BaseController {
 
     public void onConnectionButtonClick() {
-        // todo : connection user
-        System.out.println("Connection for user");
+        if (idField.getText().trim().isEmpty() && passwordField.getText().trim().isEmpty()) {
+            this.errorMessageField.setText("Veuillez remplir tout les champs");
+            return;
+        }
+
+        User user = null;
+        try {
+            //noinspection UnstableApiUsage
+            user = this.userDao.query(this.userDao.queryBuilder().where()
+                    .eq("credentials", this.idField.getText().trim())
+                    .and().eq("password", Hashing.sha256().hashString(this.passwordField.getText().trim(), StandardCharsets.UTF_8).toString())
+                    .prepare()).stream().findFirst().orElse(null);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        if (user == null) {
+            this.errorMessageField.setText("Utilisateur inexistant / mauvais mot de passe");
+            return;
+        }
+
+        new AuthStoreScreen(user).open();
+        ((Stage) this.errorMessageField.getScene().getWindow()).close();
     }
 }
