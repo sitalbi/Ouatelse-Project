03d1b1a64a5e8bff327d3f3ef0f544c32651718e
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-02-27 02:09:53 +0100

    (feat) First iteration for the product management screen.

---
+++ src/main/java/fr/s4e2/ouatelse/screens/ManagementProductScreen.java
@@ -0,0 +1,8 @@
+package fr.s4e2.ouatelse.screens;
+
+public class ManagementProductScreen extends BaseScreen {
+
+    public ManagementProductScreen() {
+        super("management_products.fxml", "Ouatelse - Menu Gestion des Produits");
+    }
+}
