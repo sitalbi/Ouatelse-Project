8e9a9f2c1799cb16a3dc592e7172b9e57c6015b3
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-02-27 02:09:53 +0100

    (feat) First iteration for the product management screen.

--- src/main/java/fr/s4e2/ouatelse/objects/ProductState.java
+++ src/main/java/fr/s4e2/ouatelse/objects/ProductState.java
@@ -1,7 +1,14 @@
 package fr.s4e2.ouatelse.objects;
 
+import org.apache.commons.lang3.text.WordUtils;
+
 public enum ProductState {
-    SUPPLY,
-    OUT,
-    WAITING_DELIVERY
+    IN_STOCK,
+    OUT_OF_STOCK,
+    WAITING_DELIVERY;
+
+    @Override
+    public String toString() {
+        return WordUtils.capitalizeFully(name().replace("_", " "));
+    }
 }
