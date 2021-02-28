43c1515f28978d96425119771402d073a1843f55
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-02-28 17:49:02 +0100

    -

---
+++ src/main/java/fr/s4e2/ouatelse/objects/Order.java
@@ -0,0 +1,26 @@
+package fr.s4e2.ouatelse.objects;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@DatabaseTable(tableName = "order")
+public class Order {
+
+    @DatabaseField(dataType = DataType.SERIALIZABLE)
+    private Map<Product, Integer> products;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private Store store;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private Vendor vendor;
+}
