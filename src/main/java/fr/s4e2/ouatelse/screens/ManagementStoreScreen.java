4cd03dd0504879d1d8339e92c9f6ad5ac2bd42a2
valeroy <valentin.leroy@etu.u-bordeaux.fr>
2021-03-01 20:07:50 +0100

    (feat) Added part of the Javadoc

--- src/main/java/fr/s4e2/ouatelse/screens/ManagementStoreScreen.java
+++ src/main/java/fr/s4e2/ouatelse/screens/ManagementStoreScreen.java
@@ -1,7 +1,13 @@
 package fr.s4e2.ouatelse.screens;
 
+/**
+ * Stores management menu
+ */
 public class ManagementStoreScreen extends BaseScreen {
 
+    /**
+     * Automatically configures the store management screen
+     */
     public ManagementStoreScreen() {
         super("management_stores.fxml", "Ouatelse - Menu Gestion des Magasins");
     }
--- src/main/java/fr/s4e2/ouatelse/screens/ManagementUserScreen.java
+++ src/main/java/fr/s4e2/ouatelse/screens/ManagementUserScreen.java
@@ -1,7 +1,13 @@
 package fr.s4e2.ouatelse.screens;
 
+/**
+ * Users management menu
+ */
 public class ManagementUserScreen extends BaseScreen {
 
+    /**
+     * Automatically configures the user management screen
+     */
     public ManagementUserScreen() {
         super("management_users.fxml", "Ouatelse - Menu Gestion des Utilisateurs");
         this.addStyleSheet("css/management.css");
