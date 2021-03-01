36bdf4b0957fcfed37fe39bf13b59a691c14b12d
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-03-01 19:01:39 +0100

    (feat) Product listing setup

--- src/main/java/fr/s4e2/ouatelse/objects/Product.java
+++ src/main/java/fr/s4e2/ouatelse/objects/Product.java
@@ -3,6 +3,8 @@ package fr.s4e2.ouatelse.objects;
 import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +14,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @DatabaseTable(tableName = "product")
 public class Product {
+
+    @DatabaseField(generatedId = true)
+    private long id;
 
     @DatabaseField(unique = true, canBeNull = false)
     private long reference;
@@ -35,11 +40,36 @@ public class Product {
     private ProductState state;
 
     @DatabaseField(canBeNull = false)
-    private long quantity = 0;
-
-    @DatabaseField(canBeNull = false)
     private String category;
 
     @DatabaseField(foreign = true, foreignAutoRefresh = true)
     private Vendor soldBy;
+
+    // Obligatory due to database mapping ########################
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private Store store;
+    // ###########################################################
+
+    @Getter
+    public static class ProductTree extends RecursiveTreeObject<ProductTree> {
+        private final LongProperty reference;
+        private final StringProperty name;
+        private final DoubleProperty sellingPrice;
+        private final DoubleProperty purchasePrice;
+        private final StringProperty brand;
+        private final StringProperty state;
+        private final StringProperty category;
+        private final StringProperty soldByName;
+
+        public ProductTree(Long reference, String name, Double sellingPrice, Double purchasePrice, String brand, String state, String category, String soldByName) {
+            this.reference = new SimpleLongProperty(reference);
+            this.name = new SimpleStringProperty(name);
+            this.sellingPrice = new SimpleDoubleProperty(sellingPrice);
+            this.purchasePrice = new SimpleDoubleProperty(purchasePrice);
+            this.brand = new SimpleStringProperty(brand);
+            this.state = new SimpleStringProperty(state);
+            this.category = new SimpleStringProperty(category);
+            this.soldByName = new SimpleStringProperty(soldByName != null ? soldByName : "");
+        }
+    }
 }
