66163fb15813afb0eacba5970b33c2a519e78d4f
valeroy <valentin.leroy@etu.u-bordeaux.fr>
2021-03-01 23:29:53 +0100

    (feat) Javadoc is almost finished for the whole "Documentation" branch

--- src/main/java/fr/s4e2/ouatelse/objects/Civility.java
+++ src/main/java/fr/s4e2/ouatelse/objects/Civility.java
@@ -2,11 +2,19 @@ package fr.s4e2.ouatelse.objects;
 
 import org.apache.commons.lang3.text.WordUtils;
 
+/**
+ * The Civilility enumeration is used to determine the gender of a person
+ */
 public enum Civility {
     M,
     MME,
     AUTRE;
 
+    /**
+     * Allows to capitalize the first letter of the first name and last name
+     *
+     * @return the converted name
+     */
     @Override
     public String toString() {
         return WordUtils.capitalizeFully(this.name()) + ".";
--- src/main/java/fr/s4e2/ouatelse/objects/ProductState.java
+++ src/main/java/fr/s4e2/ouatelse/objects/ProductState.java
@@ -1,5 +1,8 @@
 package fr.s4e2.ouatelse.objects;
 
+/**
+ * The ProductState enumeration lists the possible states of a product
+ */
 public enum ProductState {
     SUPPLY,
     OUT,
