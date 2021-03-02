6e1bc0c40a7f6e7ed6d45ce178489f69be798c7c
valeroy <valentin.leroy@etu.u-bordeaux.fr>
2021-03-02 10:17:16 +0100

    (doc) Inherit

--- src/main/java/fr/s4e2/ouatelse/controllers/ManagementRoleController.java
+++ src/main/java/fr/s4e2/ouatelse/controllers/ManagementRoleController.java
@@ -37,10 +37,12 @@ public class ManagementRoleController extends BaseController {
     private Role currentRole = null;
 
     /**
-     * Called to initialize a controller after its root element has been completely processed
+     * Initializes the controller
      *
-     * @param location {@inheritDoc}
-     * @param resources {@inheritDoc}
+     * @param location  The location used to resolve relative paths for the root object,
+     *                  or null if the location is not known.
+     * @param resources The resources used to localize the root object,
+     *                  or null if the location is not known.
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
