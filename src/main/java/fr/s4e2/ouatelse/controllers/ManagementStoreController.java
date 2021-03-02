14de87b114602465f4d201474b00ebec19997105
valeroy <valentin.leroy@etu.u-bordeaux.fr>
2021-03-02 10:17:16 +0100

    (doc) Inherit

--- src/main/java/fr/s4e2/ouatelse/controllers/ManagementStoreController.java
+++ src/main/java/fr/s4e2/ouatelse/controllers/ManagementStoreController.java
@@ -43,10 +43,12 @@ public class ManagementStoreController extends BaseController {
     private Store currentStore;
 
     /**
-     * Called to initialize a controller after its root element has been completely processed
+     * Initializes the controller
      *
-     * @param location {@inheritDoc}
-     * @param resources {@inheritDoc}
+     * @param location  The location used to resolve relative paths for the root object,
+     *                  or null if the location is not known.
+     * @param resources The resources used to localize the root object,
+     *                  or null if the location is not known.
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
--- src/main/java/fr/s4e2/ouatelse/controllers/ManagementUserController.java
+++ src/main/java/fr/s4e2/ouatelse/controllers/ManagementUserController.java
@@ -77,9 +77,12 @@ public class ManagementUserController extends BaseController {
     private User currentUser;
 
     /**
-     * Called to initialize a controller after its root element has been completely processed
-     * @param location {@inheritDoc}
-     * @param resources {@inheritDoc}
+     * Initializes the controller
+     *
+     * @param location  The location used to resolve relative paths for the root object,
+     *                  or null if the location is not known.
+     * @param resources The resources used to localize the root object,
+     *                  or null if the location is not known.
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
