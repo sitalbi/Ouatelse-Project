1e1911f20232c44b9e4c13e439c84c762bcd3a1c
valeroy <valentin.leroy@etu.u-bordeaux.fr>
2021-03-08 12:02:49 +0100

    (feat) Added Client Controller and Screen

---
+++ src/main/java/fr/s4e2/ouatelse/screens/ManagementClientScreen.java
@@ -0,0 +1,7 @@
+package fr.s4e2.ouatelse.screens;
+
+public class ManagementClientScreen extends BaseScreen {
+    public ManagementClientScreen() {
+        super("management_clients.fxml", "Ouatelse - Menu Gestion des Clients");
+    }
+}
