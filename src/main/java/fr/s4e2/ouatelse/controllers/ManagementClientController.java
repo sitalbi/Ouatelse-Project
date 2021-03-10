382244d2cef54820bee59ae2833ffd0193f84934
sitalbi <simon0111@live.fr>
2021-03-10 15:02:22 +0100

    (feat) Added management_clients.fxml and ManagementClientController.java

---
+++ src/main/java/fr/s4e2/ouatelse/controllers/ManagementClientController.java
@@ -0,0 +1,21 @@
+package fr.s4e2.ouatelse.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class ManagementClientController {
+    public void onPreferences(ActionEvent actionEvent) {
+    }
+
+    public void onQuit(ActionEvent actionEvent) {
+    }
+
+    public void onAbout(ActionEvent actionEvent) {
+    }
+
+    public void onConfirmButtonClick(MouseEvent mouseEvent) {
+    }
+
+    public void onDeleteButtonClick(MouseEvent mouseEvent) {
+    }
+}
