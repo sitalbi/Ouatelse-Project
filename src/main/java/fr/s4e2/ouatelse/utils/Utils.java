2dbb74445b9a5f2a5e72e4ad0404f65d941b1ff1
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-03-13 23:16:21 +0100

    (fixes, cleanup, feat) Overall code clean up & added the ability to escape from a selected item in a table or view from anywhere on the screen.

--- src/main/java/fr/s4e2/ouatelse/utils/Utils.java
+++ src/main/java/fr/s4e2/ouatelse/utils/Utils.java
@@ -1,12 +1,16 @@
 package fr.s4e2.ouatelse.utils;
 
+import javafx.util.StringConverter;
+
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Utils {
 
-    private Utils() {}
+    private Utils() {
+    }
 
     public static Integer getNumber(String s) {
         try {
@@ -22,5 +26,23 @@ public class Utils {
 
     public static Date localDateToDate(LocalDate dateToConvert) {
         return Date.from(dateToConvert.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static StringConverter<LocalDate> getDateConverter() {
+        return new StringConverter<LocalDate>() {
+            private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            @Override
+            public String toString(LocalDate localDate) {
+                if (localDate == null) return "";
+                return dateTimeFormatter.format(localDate);
+            }
+
+            @Override
+            public LocalDate fromString(String dateString) {
+                if (dateString == null || dateString.trim().isEmpty()) return null;
+                return LocalDate.parse(dateString, dateTimeFormatter);
+            }
+        };
     }
 }
