989536728e4b67321c41f81c63871b14485f5315
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-03-21 13:49:42 +0100

    (feat) Progress.

--- src/main/java/fr/s4e2/ouatelse/controllers/ManagementSalesController.java
+++ src/main/java/fr/s4e2/ouatelse/controllers/ManagementSalesController.java
@@ -1,4 +1,23 @@
 package fr.s4e2.ouatelse.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controller for the {@link fr.s4e2.ouatelse.screens.ManagementSalesScreen}
+ */
 public class ManagementSalesController extends BaseController {
+
+    /**
+     * Initializes the controller
+     *
+     * @param location  The location used to resolve relative paths for the root object,
+     *                  or null if the location is not known.
+     * @param resources The resources used to localize the root object,
+     *                  or null if the location is not known.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        super.initialize(location, resources);
+    }
 }
