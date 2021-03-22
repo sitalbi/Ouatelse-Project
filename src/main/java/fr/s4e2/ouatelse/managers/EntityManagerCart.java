ddd03ba870528cbe7b94b3b31b93c04279c5872c
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-03-22 15:27:56 +0100

    (feat) Added more basic functions to sales management controller + added EntityManagerCart + added EntityManagerClientStock

---
+++ src/main/java/fr/s4e2/ouatelse/managers/EntityManagerCart.java
@@ -0,0 +1,146 @@
+package fr.s4e2.ouatelse.managers;
+
+import com.j256.ormlite.dao.CloseableIterator;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.support.ConnectionSource;
+import fr.s4e2.ouatelse.exceptions.DatabaseInitialisationException;
+import fr.s4e2.ouatelse.objects.Cart;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EntityManagerCart {
+    private static final String CART_MANAGER_NOT_INITIALIZED = "EntityManagerCart could not be initialized";
+
+    private final ConnectionSource connectionSource;
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+    private final Dao<Cart, Long> instance;
+
+    /**
+     * Instantiates a new EntityManagerCart
+     *
+     * @param connectionSource the connection source
+     */
+    public EntityManagerCart(ConnectionSource connectionSource) {
+        this.connectionSource = connectionSource;
+        try {
+            this.instance = DaoManager.createDao(this.connectionSource, Cart.class);
+        } catch (SQLException exception) {
+            this.logger.log(Level.SEVERE, CART_MANAGER_NOT_INITIALIZED);
+            throw new DatabaseInitialisationException(CART_MANAGER_NOT_INITIALIZED);
+        }
+    }
+
+    /**
+     * Inserts a cart in the database
+     *
+     * @param cart the cart to be inserted
+     */
+    public void create(Cart cart) {
+        try {
+            this.instance.create(cart);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Deletes a cart from the database
+     *
+     * @param cart the cart to be deleted
+     */
+    public void delete(Cart cart) {
+        try {
+            this.instance.delete(cart);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates a cart in the database
+     *
+     * @param cart the cart to be updated
+     */
+    public void update(Cart cart) {
+        try {
+            this.instance.update(cart);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets all the carts in the database
+     *
+     * @return an iterator over all the carts in the database
+     */
+    public CloseableIterator<Cart> getAll() {
+        return this.instance.iterator();
+    }
+
+    /**
+     * Execute a prepared query
+     *
+     * @param query the prepared query
+     * @return the list of results
+     */
+    public List<Cart> executeQuery(PreparedQuery<Cart> query) {
+        List<Cart> results = null;
+
+        try {
+            results = this.instance.query(query);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return results;
+    }
+
+    /**
+     * Gets all the carts
+     *
+     * @return all the carts that are in the database
+     */
+    public List<Cart> getQueryForAll() {
+        List<Cart> results = null;
+
+        try {
+            results = this.instance.queryForAll();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return results;
+    }
+
+    /**
+     * Gets a query builder
+     *
+     * @return the query builder for the clients
+     */
+    public QueryBuilder<Cart, Long> getQueryBuilder() {
+        return this.instance.queryBuilder();
+    }
+
+    /**
+     * Check if a cart exists in the database
+     *
+     * @param cart the cart to be checked
+     * @return true if it exists, else false
+     */
+    public boolean exists(Cart cart) {
+        if (cart == null) return false;
+
+        try {
+            return this.instance.queryForId(cart.getId()) != null;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ src/main/java/fr/s4e2/ouatelse/managers/EntityManagerClientStock.java
@@ -0,0 +1,146 @@
+package fr.s4e2.ouatelse.managers;
+
+import com.j256.ormlite.dao.CloseableIterator;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.support.ConnectionSource;
+import fr.s4e2.ouatelse.exceptions.DatabaseInitialisationException;
+import fr.s4e2.ouatelse.objects.ClientStock;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EntityManagerClientStock {
+    private static final String CLIENT_STOCK_MANAGER_NOT_INITIALIZED = "EntityManagerClientStock could not be initialized";
+
+    private final ConnectionSource connectionSource;
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+    private final Dao<ClientStock, Long> instance;
+
+    /**
+     * Instantiates a new EntityManagerClientStock
+     *
+     * @param connectionSource the connection source
+     */
+    public EntityManagerClientStock(ConnectionSource connectionSource) {
+        this.connectionSource = connectionSource;
+        try {
+            this.instance = DaoManager.createDao(this.connectionSource, ClientStock.class);
+        } catch (SQLException exception) {
+            this.logger.log(Level.SEVERE, CLIENT_STOCK_MANAGER_NOT_INITIALIZED);
+            throw new DatabaseInitialisationException(CLIENT_STOCK_MANAGER_NOT_INITIALIZED);
+        }
+    }
+
+    /**
+     * Inserts a client stock in the database
+     *
+     * @param clientStock the client stock to be inserted
+     */
+    public void create(ClientStock clientStock) {
+        try {
+            this.instance.create(clientStock);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Deletes a client stock from the database
+     *
+     * @param clientStock the client stock to be deleted
+     */
+    public void delete(ClientStock clientStock) {
+        try {
+            this.instance.delete(clientStock);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates a client stock in the database
+     *
+     * @param clientStock the client stock to be updated
+     */
+    public void update(ClientStock clientStock) {
+        try {
+            this.instance.update(clientStock);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets all the client stocks in the database
+     *
+     * @return an iterator over all the client stocks in the database
+     */
+    public CloseableIterator<ClientStock> getAll() {
+        return this.instance.iterator();
+    }
+
+    /**
+     * Execute a prepared query
+     *
+     * @param query the prepared query
+     * @return the list of results
+     */
+    public List<ClientStock> executeQuery(PreparedQuery<ClientStock> query) {
+        List<ClientStock> results = null;
+
+        try {
+            results = this.instance.query(query);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return results;
+    }
+
+    /**
+     * Gets all the client stocks
+     *
+     * @return all the client stocks that are in the database
+     */
+    public List<ClientStock> getQueryForAll() {
+        List<ClientStock> results = null;
+
+        try {
+            results = this.instance.queryForAll();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return results;
+    }
+
+    /**
+     * Gets a query builder
+     *
+     * @return the query builder for the clients
+     */
+    public QueryBuilder<ClientStock, Long> getQueryBuilder() {
+        return this.instance.queryBuilder();
+    }
+
+    /**
+     * Check if a client stock exists in the database
+     *
+     * @param clientStock the client stock to be checked
+     * @return true if it exists, else false
+     */
+    public boolean exists(ClientStock clientStock) {
+        if (clientStock == null) return false;
+
+        try {
+            return this.instance.queryForId(clientStock.getId()) != null;
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            return false;
+        }
+    }
+}
