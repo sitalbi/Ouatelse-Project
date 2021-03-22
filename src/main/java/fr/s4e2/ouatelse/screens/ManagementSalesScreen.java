1bab91362327ad3f6ca4a1b77b027583bfd96863
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-03-22 12:03:30 +0100

    (feat) Backbone for sales management

--- src/main/java/fr/s4e2/ouatelse/screens/ManagementSalesScreen.java
+++ src/main/java/fr/s4e2/ouatelse/screens/ManagementSalesScreen.java
@@ -1,11 +1,13 @@
 package fr.s4e2.ouatelse.screens;
 
+import fr.s4e2.ouatelse.objects.Store;
+
 /**
  * Sales Management Screen
  */
 public class ManagementSalesScreen extends BaseScreen {
 
-    public ManagementSalesScreen() {
-        super("management_sales.fxml", "Menu Gestion des Ventes");
+    public ManagementSalesScreen(Store authentificationStore) {
+        super("management_sales.fxml", "Menu Gestion des Ventes", authentificationStore);
     }
 }
