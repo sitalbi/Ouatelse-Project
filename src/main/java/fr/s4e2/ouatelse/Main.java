909f3842a4b1903ad67ce29e743b2813f3409b4c
scaillat <simon.caillat@etu.u-bordeaux.fr>
2021-03-23 11:53:19 +0100

    (fix) Quick Fix (Wrong Screen startup)

--- src/main/java/fr/s4e2/ouatelse/Main.java
+++ src/main/java/fr/s4e2/ouatelse/Main.java
@@ -1,7 +1,7 @@
 package fr.s4e2.ouatelse;
 
 import fr.s4e2.ouatelse.managers.DatabaseManager;
-import fr.s4e2.ouatelse.screens.ManagementSalaryScreen;
+import fr.s4e2.ouatelse.screens.AuthUserScreen;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import lombok.Getter;
@@ -34,6 +34,6 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
-        new ManagementSalaryScreen().open();
+        new AuthUserScreen().open();
     }
 }
