4b85ff20d17cbce95cc225182c88929ea3526c9c
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-03-30 23:50:12 +0200

    (clean) Cleaned repo before release

--- src/main/java/fr/s4e2/ouatelse/screens/ManagementPlannedOrdersScreen.java
+++
@@ -1,9 +0,0 @@
-package fr.s4e2.ouatelse.screens;
-
-import fr.s4e2.ouatelse.objects.Store;
-
-public class ManagementPlannedOrdersScreen extends BaseScreen {
-    public ManagementPlannedOrdersScreen(Store store) {
-        super("management_planned_orders.fxml", "Menu Programmation des commandes", store);
-    }
-}
