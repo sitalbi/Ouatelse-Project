885dfba5712d27b5abe824c256abbcd04ae18646
mcaravati <matteo.caravati@etu.u-bordeaux.fr>
2021-03-30 23:50:12 +0200

    (clean) Cleaned repo before release

--- src/main/java/fr/s4e2/ouatelse/screens/StatisticsSalesScreen.java
+++
@@ -1,9 +0,0 @@
-package fr.s4e2.ouatelse.screens;
-
-import fr.s4e2.ouatelse.objects.Store;
-
-public class StatisticsSalesScreen extends BaseScreen {
-    public StatisticsSalesScreen(Store authentificationStore) {
-        super("statistics_sales.fxml", "Statistiques des Ventes", authentificationStore);
-    }
-}
